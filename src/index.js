ffdedbfa9e84affc86ef419db9216674ce70ae79
zufanayalew135 <156121940+zufanayalew135@users.noreply.github.com>
2024-02-23 06:42:11 -0800

    Added the search functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function updateWeatherInfo(response) {
+  let tempratureElement = document.querySelector("#temprature");
+  let temprature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  tempratureElement.innerHTML = Math.round(temprature);
+}
+
+function searchCity(city) {
+  let apikey = "4a5fa8537ac0d63c3tacbe1af0b6b4co";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function click(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
