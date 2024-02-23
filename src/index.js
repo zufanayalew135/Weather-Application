43ef86137b22dd19aa499341e75018d90a0f0d5b
zufanayalew135 <156121940+zufanayalew135@users.noreply.github.com>
2024-02-23 09:56:01 -0800

    injected weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ function updateWeatherInfo(response) {
   let tempratureElement = document.querySelector("#temprature");
   let temprature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let iconElement = document.querySelector("#icon");
+
+  console.log(iconElement);
 
   cityElement.innerHTML = response.data.city;
   tempratureElement.innerHTML = Math.round(temprature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${wind}km/h`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" />`;
 }
 
 function searchCity(city) {
