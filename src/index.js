73fc98b3efa4163a643d38ff6707665a8c05152b
zufanayalew135 <156121940+zufanayalew135@users.noreply.github.com>
2024-02-22 22:40:24 -0800

    created the index,js file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function click(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", click);
